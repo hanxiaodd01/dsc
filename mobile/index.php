38c49deaa0b2d03b4989b12830b6afddca2a58f0
hanxiaodd01 <hanxiao@hk01.com>
2019-10-31 18:21:18 +0800

    init: update composer.json of / and mobile/ , and autoload

--- mobile/index.php
+++ mobile/index.php
@@ -6,7 +6,7 @@ define('APP_DEBUG', true);
 require_once __DIR__ . '/app/Support/helpers.php';
 require_once __DIR__ . '/thinker/Common/functions.prepend.php';
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 require __DIR__ . '/bootstrap/think.php';
 
